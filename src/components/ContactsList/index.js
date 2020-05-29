b5fc72381aaca8c25357a1eeb8adac7b84822021
Hamed Farag <hamed.farag.2009@gmail.com>
2020-05-29 17:39:15 +0200

    Move ContactsList Logic from App to ContactsList Component

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -2,18 +2,82 @@ import React from "react";
 
 import Contact from "../Contact";
 
-export default function ContactsList(props) {
-  const { data, actions } = props;
+import { getContacts, deleteContact } from "../../utils/ContactsAPI";
 
-  const onDeleteContact = (contact) => {
-    actions.handleDeleteContact(contact);
+export default class ContactsList extends React.Component {
+  state = {
+    contacts: [],
+    query: "",
   };
 
-  return data.contacts.map((contact) => (
-    <Contact
-      key={contact.id}
-      data={{ contact }}
-      actions={{ handleDeleteContact: onDeleteContact }}
-    />
-  ));
+  handleQuery = (query) => {
+    this.setState({
+      query: query.trim(),
+    });
+  };
+
+  componentDidMount() {
+    getContacts().then((response) => {
+      this.setState({
+        contacts: response.contacts,
+      });
+    });
+  }
+
+  handleDeleteContact = (contact) => {
+    this.setState(
+      (prevState) => ({
+        contacts: prevState.contacts.filter((cont) => cont.id !== contact.id),
+      }),
+      () => {
+        deleteContact(contact);
+      }
+    );
+  };
+
+  onDeleteContact = (contact) => {
+    this.handleDeleteContact(contact);
+  };
+
+  render() {
+    const { contacts, query } = this.state;
+
+    const showingContacts =
+      query === ""
+        ? contacts
+        : contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(query.toLowerCase())
+          );
+
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            type="text"
+            value={query}
+            className="search-contacts"
+            placeholder="Search for contact"
+            onChange={(e) => this.handleQuery(e.target.value)}
+          />
+        </div>
+        {showingContacts.length !== contacts.length && (
+          <div className="showing-contacts">
+            <span>
+              Now Showing {showingContacts.length} of {contacts.length}
+            </span>
+            <button onClick={() => this.handleQuery("")}>
+              Show all contacts
+            </button>
+          </div>
+        )}
+        {showingContacts.map((contact) => (
+          <Contact
+            key={contact.id}
+            data={{ contact }}
+            actions={{ handleDeleteContact: this.onDeleteContact }}
+          />
+        ))}
+      </div>
+    );
+  }
 }
